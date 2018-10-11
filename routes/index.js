108cd53d8bf6634af19a11af831727339ab2b653
oscar <oscar@o2webdev.com>
2018-10-11 13:53:21 -0400

    fixed routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+const path = require("path");
+const router = require("express").Router();
+// const apiRoutes = require("./api");
+
+// router.use("/", apiRoutes);
+
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
